0bc1bfdff0abc638ce84b20215033f56cc27fbaf
bytrix <wengzhijie@126.com>
2017-04-28 19:00:34 +0800

    Add judgment in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,22 @@
 TERMUX_HOME="/data/data/com.termux/files"
-command_java=`ln -s $PWD/java $TERMUX_HOME/usr/bin/java`
-command_javac=`ln -s $PWD/javac $TERMUX_HOME/usr/bin/javac`
-echo "Linking java to $TERMUX_HOME/usr/bin/java"
-exec $command_java
-echo "Complete linking java to $TERMUX_HOME/usr/bin/java"
-echo "Linking javac to $TERMUX_HOME/usr/bin/javac"
-exec $command_javac
-echo "Complete linking javac to $TERMUX_HOME/usr/bin/javac"
+BIN="$TERMUX_HOME/usr/bin"
+
+if [ `which ecj` == "" ];then
+	apt-get install ecj
+fi
+
+if [ `which dx` == "" ];then
+	apt-get install dx
+fi
+
+if [ ! -f "$BIN/java" ];then
+	command_java=`ln -s $PWD/java $BIN/java`
+	echo "Linking java to $TERMUX_HOME/usr/bin/java"
+	exec $command_java
+fi
+
+if [ ! -f "$BIN/javac"];then
+	command_javac=`ln -s $PWD/javac $BIN/javac`
+	echo "Linking javac to $TERMUX_HOME/usr/bin/javac"
+	exec $command_javac
+fi
